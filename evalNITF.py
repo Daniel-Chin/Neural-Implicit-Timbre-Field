8dc3ab959bb5a9ab97fb714227db6691ac08f4a6
Daniel Chin <daniel.chin@nyu.edu>
2023-05-11 10:40:20 +0800

    tkinter works

--- evalNITF.py
+++ evalNITF.py
@@ -5,6 +5,13 @@ import torch
 from torchWork import loadExperiment, DEVICE
 from torchWork.experiment_control import EXPERIMENT_PY_FILENAME, loadLatestModels
 import tkinter as tk
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg,
+    NavigationToolbar2Tk, 
+)
 
 from prepare import *
 from exp_group import ExperimentGroup
@@ -38,48 +45,62 @@ class RightFrame(tk.Frame):
             self.columnconfigure(1, weight=1)
             self.columnconfigure(2, weight=0)
             self.columnconfigure(3, weight=1)
+            self.rowconfigure(0, weight=1)
 
             tk.Label(self, text='rand_init_i:').grid(
-                row=0, column=0, 
+                row=0, column=0, sticky=tk.NSEW, 
             )
             tk.Spinbox(
                 self, textvariable=rand_init_i, 
-                from_=0, to=1e8, 
+                from_=-1, to=1e8, 
                 command=resynth, 
             ).grid(
-                row=0, column=1, 
+                row=0, column=1, sticky=tk.NSEW, 
             )
 
             tk.Label(self, text='epoch:').grid(
-                row=0, column=2,  
+                row=0, column=2,  sticky=tk.NSEW, 
             )
             tk.Spinbox(
                 self, textvariable=epoch, 
-                from_=0, to=1e8, 
+                from_=-1, to=1e8, 
                 command=resynth, 
             ).grid(
-                row=0, column=3, 
+                row=0, column=3, sticky=tk.NSEW, 
             )
     
     class SquaresFrame(tk.Frame):
         class SpectralEnvelopeFrame(tk.Frame):
             def __init__(self, parent) -> None:
                 super().__init__(parent)
+
+                fig = Figure(figsize=(.2, .1), dpi=100)
+                figure_canvas = FigureCanvasTkAgg(fig, self)
+                NavigationToolbar2Tk(figure_canvas, self)
+                ax = fig.add_subplot()
+                ax.plot([1,3,2,4,10,5])
+                fig.tight_layout()
+                figure_canvas.get_tk_widget().pack(
+                    side=tk.TOP, fill=tk.BOTH, expand=True, 
+                )
         
         def __init__(self, parent) -> None:
             super().__init__(parent)
 
             self.columnconfigure(0, weight=1)
-            self.columnconfigure(1, weight=2)
+            self.columnconfigure(1, weight=1)
+            self.rowconfigure(0, weight=1)
 
-            touchPad = tk.Frame(self)
+            touchPad = tk.Label(
+                self, text='touch pad', background='#ddffff', 
+            )
             touchPad.grid(
-                row=0, column=0, 
+                row=0, column=0, sticky=tk.NSEW, 
             )
             touchPad.bind('<Motion>', self.onMotion)
 
             self.SpectralEnvelopeFrame(self).grid(
-                row=0, column=1, 
+                row=0, column=1, sticky=tk.NSEW, 
             )
         
         def onMotion(self, event: tk.Event):
@@ -133,24 +154,25 @@ class RightFrame(tk.Frame):
         self.rowconfigure(1, weight=1)
         self.rowconfigure(2, weight=0)
         self.rowconfigure(3, weight=0)
+        self.columnconfigure(0, weight=1)
 
         self.SpinsFrame(
             self, resynth, rand_init_i, epoch, 
         ).grid(
-            row=0, column=0, 
+            row=0, column=0, sticky=tk.NSEW, 
         )
         self.SquaresFrame(self).grid(
-            row=1, column=0, 
+            row=1, column=0, sticky=tk.NSEW, 
         )
         self.PitchFrame(
             self, resynth, pitch, 
         ).grid(
-            row=2, column=0, 
+            row=2, column=0, sticky=tk.NSEW, 
         )
         self.AmpFrame(
             self, resynth, amp, 
         ).grid(
-            row=3, column=0, 
+            row=3, column=0, sticky=tk.NSEW, 
         )
 
 def initRoot(
@@ -162,12 +184,13 @@ def initRoot(
 
     root.columnconfigure(0, weight=1)
     root.columnconfigure(1, weight=3)
+    root.rowconfigure(0, weight=1)
     
     leftFrame = LeftFrame(
         root, groups, resynth, group_selection, 
     )
     leftFrame.grid(
-        row=0, column=0, 
+        row=0, column=0, sticky=tk.NSEW, 
     )
 
     rightFrame = RightFrame(
@@ -175,7 +198,7 @@ def initRoot(
         resynth, pitch, amp, rand_init_i, epoch, 
     )
     rightFrame.grid(
-        row=0, column=1, 
+        row=0, column=1, sticky=tk.NSEW, 
     )
 
 def loadNITF(group, rand_init_i, epoch):
@@ -197,11 +220,11 @@ def main():
 
         root = tk.Tk()
 
-        group_selection = tk.IntVar()
-        pitch = tk.DoubleVar()
-        amp = tk.DoubleVar()
-        rand_init_i = tk.StringVar()
-        epoch = tk.StringVar()
+        group_selection = tk.IntVar(root, 0)
+        pitch = tk.DoubleVar(root, 60)
+        amp = tk.DoubleVar(root, .001)
+        rand_init_i = tk.StringVar(root, 0)
+        epoch = tk.StringVar(root, 0)
 
         def resynth(*args):
             ...
