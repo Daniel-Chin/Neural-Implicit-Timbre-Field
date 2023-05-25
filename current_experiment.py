cfe3b1ba5de8e97a0c0c2ee6dbd35209420cfa6e
Daniel Chin <daniel.chin@nyu.edu>
2023-05-25 15:28:00 +0800

    exp

--- current_experiment.py
+++ current_experiment.py
@@ -13,7 +13,7 @@ from dataset_definitions import voiceScaleF0IsLatent as datasetDef
 SLOW_EVAL_EPOCH_INTERVAL = 1
 
 EXP_NAME = 'fast_lr'
-N_RAND_INITS = 1
+N_RAND_INITS = 6
 dataset = MyDataset(datasetDef)
 
 class MyExpGroup(ExperimentGroup):
@@ -56,9 +56,15 @@ template.max_epoch = 1e5
 template.lossWeightTree['dredge_regularize'].weight = 1e-5
 template.nif_abs_out = True
 template.nif_abs_confidence = True
+template.nif_fast_lr = 2e-2
+template.max_epoch = 1e3
 
-for lr in [3e-4, 1e-3, 3e-3, 1e-2]:
-    hP = deepcopy(template)
-    template.nif_fast_lr = lr
-    hP.ready(globals())
-    GROUPS.append(MyExpGroup(hP))
+# for lr in [3e-4, 1e-3, 3e-3, 1e-2]:
+#     hP = deepcopy(template)
+#     hP.nif_fast_lr = lr
+#     hP.ready(globals())
+#     GROUPS.append(MyExpGroup(hP))
+
+hP = deepcopy(template)
+hP.ready(globals())
+GROUPS.append(MyExpGroup(hP))
--- workspace.py
+++ workspace.py
@@ -6,23 +6,23 @@ from shared import *
 
 EXP_PATH = path.join('./experiments/',
 '''
-2023_m05_d24@08_31_20_multi_page
+2023_m05_d25@02_16_26_fast_lr
 '''
 .strip())
 
 def EPOCHS(experiment):
     start = 0
     # start = 1000
-    # step = experiment.SLOW_EVAL_EPOCH_INTERVAL
-    step = 16
+    step = experiment.SLOW_EVAL_EPOCH_INTERVAL
+    # step = 16
     real_start = round(start / step) * step
-    # return count(real_start, step)
+    return count(real_start, step)
     # return range(real_start, real_start + step * 8, step)
-    return range(real_start, 3000, step)
+    # return range(real_start, 3000, step)
 
 # TIME_SLICE = slice(None)
 TIME_SLICE = slice(0, 256)
-# SELECT_PAGE = round(.25 * SR / PAGE_LEN * 2)
-SELECT_PAGE = round(1.35 * SR / PAGE_LEN * 2)
+SELECT_PAGE = round(.25 * SR / PAGE_LEN * 2)
+# SELECT_PAGE = round(1.35 * SR / PAGE_LEN * 2)
 SELECT_GROUPS = slice(None)
 # SELECT_GROUPS = slice(-1, None)
